a21f7295401bc81377012553248b2e449b7bb837
Hamisi Ngao <newhamisi@gmail.com>
2023-01-05 19:02:21 -0600

    implement add-book feature

--- books/views.py
+++ books/views.py
@@ -2,10 +2,19 @@ from books.models import Book
 from books.serializers import BookSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 class BookListView(APIView):
     
-    def get(self, request):
+    def get(self, request, format=None):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response({"books": serializer.data}) #TODO: add additional jsonfield for count
+
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
